63843412dbe95c5c8d4aa28172cd1d63492f23ae
ilyaTheCreator <respondatanytime@gmail.com>
2022-01-20 20:12:33 +0300

    добавлено

---
+++ Ducks/Ducks/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Biblioteka;
+
+namespace Ducks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RedheadDuck readheadduck = new RedheadDuck();
+            MallardDuck mallardduck = new MallardDuck();
+
+            Duck[] ducks = new Duck[] { readheadduck, mallardduck };
+
+            foreach (var x in ducks)
+            {
+                Console.WriteLine(x.Swim());
+                Console.WriteLine(x.Display());
+                Console.WriteLine(x.Quack());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
